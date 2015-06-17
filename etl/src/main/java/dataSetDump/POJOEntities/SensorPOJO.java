99f2f09382377616db7a3a06a4a2241909e319fa
Deepak Nair <gotodeepak@gmail.com>
2015-06-17 19:17:42 +0530

    renmaed directory package ETL.loader

--- etl/src/main/java/dataSetDump/POJOEntities/SensorPOJO.java
+++ etl/src/main/java/dataSetDump/POJOEntities/SensorPOJO.java
@@ -1,7 +1,109 @@
 package dataSetDump.POJOEntities;
 
+import org.envirocar.server.core.entities.Sensor;
+import org.joda.time.DateTime;
+
+import java.util.Map;
+
 /**
  * @author deepaknair on 17/06/15 AD.
  */
-public class SensorPOJO {
+
+
+public class SensorPOJO implements Sensor {
+
+    private String type;
+    private Map<String, Object> properties;
+    private String identifier;
+    private DateTime creationTime;
+    private DateTime modificationTime;
+
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean hasType() {
+        if (this.getType() == null) {
+            return false;
+        } else
+            return true;
+    }
+
+    @Override
+    public Map<String, Object> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
+        this.properties = properties;
+    }
+
+    @Override
+    public void addProperty(String key, Object val) {
+        this.properties.put(key, val);
+    }
+
+    @Override
+    public boolean hasProperties() {
+        if (this.getProperties() == null) {
+            return false;
+        } else
+            return true;
+    }
+
+    @Override
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    @Override
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    @Override
+    public boolean hasIdentifier() {
+        if (this.getType() == null) {
+            return false;
+        } else
+            return true;
+    }
+
+    @Override
+    public DateTime getCreationTime() {
+        return this.creationTime;
+    }
+
+    @Override
+    public boolean hasCreationTime() {
+        if (this.getCreationTime() == null) {
+            return false;
+        } else
+            return true;
+    }
+
+    @Override
+    public DateTime getModificationTime() {
+        return this.modificationTime;
+    }
+
+    @Override
+    public boolean hasModificationTime() {
+        if (this.getModificationTime() == null) {
+            return false;
+        } else
+            return true;
+    }
+
+
 }
+
+
---
+++ etl/src/main/java/loader/TripleStoreLoader.java
@@ -0,0 +1,12 @@
+package loader;
+
+/**
+ * @author deepaknair on 17/06/15 AD.
+ *         A Loader which loads into a Triple Store database
+ */
+
+
+public interface TripleStoreLoader {
+
+    public String getTargetLoader();
+}
