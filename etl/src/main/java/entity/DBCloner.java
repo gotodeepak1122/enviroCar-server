0053ca6fcf217584ae9e16e0035606c8a5929571
Deepak Nair <gotodeepak@gmail.com>
2015-06-17 22:14:11 +0530

    changes in  DBcloner description

--- etl/src/main/java/entity/DBCloner.java
+++ etl/src/main/java/entity/DBCloner.java
@@ -6,11 +6,10 @@ import java.util.Date;
 
 /**
  * @author deepaknair on 17/06/15 AD.
- */
-
-/**
  * entities which clone data from Database style data sources
  */
+
+
 public interface DBCloner extends Cloner {
 
     public Date getLastExecutionTime();
